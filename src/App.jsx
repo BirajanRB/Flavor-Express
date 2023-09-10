14b8efe10698144504cc3165f1bae4903363f92f
birajan <birajanrb@gmail.com>
2023-09-10 23:37:44 +0545

    Flavor-Express Initial App-Structure

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/homePage/HomePage";
+import LoginPage from "./components/loginPage/LoginPage";
+import RegistrationPage from "./components/registrationPage/RegistrationPage";
+import RestaurantsPage from "./components/restaurantsPage/RestaurantsPage";
+import RestaurantAboutPage from "./components/restaurantAboutPage/RestaurantAboutPage";
+import FoodItemsPage from "./components/foodItemsPage/FoodItemsPage";
+import CartPage from "./components/cartPage/CartPage";
+
+function App() {
+  return (
+    <div className="appContainer">
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/loginPage" element={<LoginPage />}></Route>
+        <Route path="/registrationPage" element={<RegistrationPage />}></Route>
+        <Route path="/restaurantsPage" element={<RestaurantsPage />}></Route>
+        <Route path="/restaurantAboutPage" element={<RestaurantAboutPage />} />
+        <Route path="/foodItemsPage" element={<FoodItemsPage />}></Route>
+        <Route path="/cartPage" element={<CartPage />}></Route>
+        <Route path="*" element={<h1>PageNotFound! :(</h1>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
