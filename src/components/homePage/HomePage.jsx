c9d9e2bca64b60f491f83774324b7557cb5fa94d
Samikshya Chapagain <csamikshya21@tbc.edu.np>
2023-09-14 08:40:29 +0545

    login & Registration

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function HomePage() {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <button>
+        <NavLink to="/RegistrationPage"></NavLink>Sign up
+      </button>
+    </div>
+  );
 }
